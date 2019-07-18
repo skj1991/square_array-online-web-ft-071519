afdb762697dddb0d636efd7c7fd65d5717bf5910
Stacy Johns <stacykjohns@icloud.com>
2019-07-18 15:14:13 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  # your code here
+  new_numbers = []
+  counter = 0
+  while counter < array.length do
+    new_numbers << array[counter]**2
+    counter += 1
+  end
+  return new_numbers
 end
